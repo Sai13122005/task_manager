c975cf202f1ce405a59c809571cdd983a892f048
Sai13122005 <pentasaikrishna00@gmail.com>
2026-05-21 16:17:39 +0530

    Fixed Swagger white page issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,9 +5,11 @@ const authRoutes = require("./routes/authRoutes");
 const testRoutes = require("./routes/testRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 const taskRoutes = require("./routes/taskRoutes");
-const errorHandler = require("./middleware/errorMiddleware");
+
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./config/swagger");
+
+const errorHandler = require("./middleware/errorMiddleware");
 
 const app = express();
 
@@ -15,7 +17,6 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(errorHandler);
 
 
 // Routes
@@ -24,17 +25,24 @@ app.use("/api/v1/test", testRoutes);
 app.use("/api/v1/admin", adminRoutes);
 app.use("/api/v1/tasks", taskRoutes);
 
+
+// Swagger
 app.use(
     "/api-docs",
     swaggerUi.serve,
     swaggerUi.setup(swaggerSpec)
 );
 
-// Test Route
+
+// Root Route
 app.get("/", (req, res) => {
     res.json({
         message: "API Running"
     });
 });
+
+
+// Error Middleware MUST BE LAST
+app.use(errorHandler);
 
 module.exports = app;
